89e4070def8ac97162c71b5669233fd4d05577c6
panxiandiao_i <panxiandiao_i@didiglobal.com>
2020-03-01 17:00:26 +0800

    重构完成

---
+++ routes/user.js
@@ -0,0 +1,20 @@
+const router = require('koa-router')()
+const { login } = require('../controller/user')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+
+router.prefix('/api/user')
+
+router.post('/login', async function (ctx, next) {
+  const { username, password } = ctx.request.body
+  const data = await login(username, password)
+  if(data.username) {
+    // 设置 session
+    ctx.session.username = data.username
+    ctx.session.realname = data.realname
+    ctx.body = new SuccessModel()
+    return
+  }
+  ctx.body = new ErrorModel('登陆失败')
+})
+
+module.exports = router
